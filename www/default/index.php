84fa8db6d046b484c74ece1dd56327d352a9f630
Juan Julián Merelo Guervós <jjmerelo@gmail.com>
2019-10-02 18:16:55 +0200

    Initial commit

---
+++ www/default/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * If a custom dashboard file exists, load that instead of the default
+ * dashboard provided by Varying Vagrant Vagrants. This file should be
+ * located in the `www/default/` directory.
+ */
+if ( file_exists( 'dashboard-custom.php' ) ) {
+	include( 'dashboard-custom.php' );
+	exit;
+}
+
+if ( file_exists( 'dashboard/dashboard-default.php' ) ) {
+	include( 'dashboard/dashboard-default.php');
+	exit;
+}
+?>
+<h2>VVV</h2>
+<p>VVV hasn't finished provisioning, and the dashboard hasn't been set up yet. Come back in a minute or two</p>
